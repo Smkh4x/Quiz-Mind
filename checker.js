9bd20fbcd380dd5aa8f694517d0766334358e234
Smkh4x <youssefalmona8@gmail.com>
2026-04-12 12:40:55 -0800

    add exercices , and create function random quiz linked with console and quiz

---
+++ checker.js
@@ -0,0 +1,13 @@
+import readline from "readline";
+import fs from "fs/promises";
+
+export function checker() {
+
+                const command = ["check", "exit"] 
+                if(command[0]){
+                    console.log("hello")
+                }/* else if (command === 1){
+
+                }*/
+
+};
--- console.js
+++ console.js
@@ -1,30 +1,44 @@
-import readline from "readline";
-import { startquiz } from "./runner.js";
+import readline, { clearLine } from "readline";
+import { RandomQuiz } from "./random.js"
+import { checker } from "./checker.js";
 
-console.log("Welcome to the Logic Question Generator!\n");
-console.log("This tool generates a random logic question for you to solve in JavaScript.\n");
+
+const colors = {
+    red: (text) => `\x1b[31m${text}\x1b[0m`,
+    yellow: (text) => `\x1b[33m${text}\x1b[0m`,
+    blue: (text) => `\x1b[34m${text}\x1b[0m`,
+    green: (text) => `\x1b[32m${text}\x1b[0m`
+}
+
+function main(){
+    console.log(colors.blue("\n\t\tWelcome to the Logic Question Generator!\n"));
+console.log(colors.yellow("This tool generates a random logic question for you to solve in JavaScript.\n"));
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-    rl.question("[+] PLease Enter any key to generate a Random Logic Question...", (answer) => {
+    rl.question("\t[+] PLease Enter any key to generate a Random Logic Question...\n", (answer) => {
         let i = 0;
 
             const loading = setInterval(() => {
                 const dots = ".".repeat(i % 4);
-                process.stdout.write(`\rGenerating question${dots}   `);
+                process.stdout.write(`\r Generating question ${dots} `);
                 i++;
             }, 500);
 
                 setTimeout(() => {
                     clearInterval(loading);
-                    console.log("Here is your random logic question:\n");
-                    startquiz();
-                    rl.close();
+                    console.log(`\n\n\n \t \t \t \t Extercice`);
+                    console.log(`${RandomQuiz()}`);
+
+
                 }, 3000);
 
-    
+
+
     });
+}
+main()
 
 
